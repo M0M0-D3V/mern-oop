25e6c344a8353fad66c668d4494363d013ad9195
Monica Hong <M0M0-D3V@users.noreply.github.com>
2020-05-02 04:33:52 -0700

    commit

--- ninja.js
+++ ninja.js
@@ -1,21 +1,22 @@
 // [x] Create a Ninja class
 class Ninja {
-  constructor(name, health, speed, strength) {
+  constructor(name, health, speed = 3, strength = 3) {
     // [x] add an attribute: name
     this.name = name;
     // [x] add an attribute: health
     this.health = health;
     // [x] add a attribute: speed - give a default value of 3
-    this.speed = 3;
-    // [] add a attribute: strength - give a default value of 3
+    this.speed = speed;
+    // [x] add a attribute: strength - give a default value of 3
+    this.strength = strength;
   }
-  // [] add a method: sayName() - This should log that Ninja's name to the console
+  // [x] add a method: sayName() - This should log that Ninja's name to the console
   sayName() {
     console.log(this.name);
   }
-  // [] add a method: showStats() - This should show the Ninja's name, strength, speed, and health.
+  // [x] add a method: showStats() - This should show the Ninja's name, strength, speed, and health.
   showStats() {}
-  // [] add a method: drinkSake() - This should add +10 Health to the Ninja
+  // [x] add a method: drinkSake() - This should add +10 Health to the Ninja
   drinkSake() {
     this.health += 10;
   }
