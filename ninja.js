0d4dc53c1d106bb7d6fd80364fda41af538baee9
Monica Hong <M0M0-D3V@users.noreply.github.com>
2020-05-02 23:14:58 -0700

    Super Ninja

--- ninja.js
+++ ninja.js
@@ -15,9 +15,38 @@ class Ninja {
     console.log(this.name);
   }
   // [x] add a method: showStats() - This should show the Ninja's name, strength, speed, and health.
-  showStats() {}
+  showStats() {
+    console.log(this.name, this.health, this.speed, this.strength);
+  }
   // [x] add a method: drinkSake() - This should add +10 Health to the Ninja
   drinkSake() {
     this.health += 10;
   }
 }
+
+const ninja1 = new Ninja("Hyabusa", 40);
+ninja1.sayName();
+ninja1.showStats();
+
+// Super Ninja Assignment
+// [x] create a class Sensei that inherits from the Ninja class
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name, 200, 10, 10);
+    // [x] add an attribute: wisdom - default to 10
+    this.wisdom = 10;
+  }
+  // [x] create a method: speakWisdom()
+  speakWisdom() {
+    // [x] create a method: drinkSake()
+    super.drinkSake();
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+    const newStats = super.showStats();
+    console.log(newStats);
+  }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
